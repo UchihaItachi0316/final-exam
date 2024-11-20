36076626bdbd8376669fd18c990dad3d6206be7e
Hans Benitez <hansbenitez26@gmail.com>
2024-11-20 22:19:41 +0800

    register students implemented

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,54 +1,50 @@
 <!-- Template Files here -->
 <?php 
     session_start();
-    if (!isset($_SESSION['email'])) {
-        header('Location: ../index.php');
-        exit();
-    }
+    require('../functions.php');
+    guard();
     include('partials/header.php');
     include('partials/side-bar.php');
  
  ?>
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
-    <h1 class="h2">Dashboard</h1>        
-    
-    <div class="row mt-5">
-        <div class="col-12 col-xl-3">
-            <div class="card border-primary mb-3">
-                <div class="card-header bg-primary text-white border-primary">Number of Subjects:</div>
-                <div class="card-body text-primary">
-                    <h5 class="card-title">0</h5>
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
+        <h1 class="h2">Dashboard</h1>        
+        <div class="row mt-5">
+            <div class="col-12 col-xl-3">
+                <div class="card border-primary mb-3">
+                    <div class="card-header bg-primary text-white border-primary">Number of Subjects:</div>
+                    <div class="card-body text-primary">
+                        <h5 class="card-title">0</h5>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-12 col-xl-3">
-            <div class="card border-primary mb-3">
-                <div class="card-header bg-primary text-white border-primary">Number of Students:</div>
-                <div class="card-body text-success">
-                    <h5 class="card-title">0</h5>
+            <div class="col-12 col-xl-3">
+                <div class="card border-primary mb-3">
+                    <div class="card-header bg-primary text-white border-primary">Number of Students:</div>
+                    <div class="card-body text-success">
+                        <h5 class="card-title">0</h5>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-12 col-xl-3">
-            <div class="card border-danger mb-3">
-                <div class="card-header bg-danger text-white border-danger">Number of Failed Students:</div>
-                <div class="card-body text-danger">
-                    <h5 class="card-title">0</h5>
+            <div class="col-12 col-xl-3">
+                <div class="card border-danger mb-3">
+                    <div class="card-header bg-danger text-white border-danger">Number of Failed Students:</div>
+                    <div class="card-body text-danger">
+                        <h5 class="card-title">0</h5>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-12 col-xl-3">
-            <div class="card border-success mb-3">
-                <div class="card-header bg-success text-white border-success">Number of Passed Students:</div>
-                <div class="card-body text-success">
-                    <h5 class="card-title">0></h5>
+            <div class="col-12 col-xl-3">
+                <div class="card border-success mb-3">
+                    <div class="card-header bg-success text-white border-success">Number of Passed Students:</div>
+                    <div class="card-body text-success">
+                        <h5 class="card-title">0></h5>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>    
-</main>
+        </div>    
+    </main>
 <?php 
     include('partials/footer.php')
-
 ?>
 <!-- Template Files here -->
